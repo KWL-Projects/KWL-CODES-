4877ead890c4bb1913edb9e799079673088b6def
LuhanPieterse <112892306+LuhanPieterse@users.noreply.github.com>
2024-09-23 20:32:46 +0200

    changes to user API

--- KWLCodesAPI/Models/user.cs
+++ KWLCodesAPI/Models/user.cs
@@ -6,6 +6,6 @@
         public int login_id {  get; set; }
         public string user_firstname { get; set; }
         public string user_lastname { get; set; }
-        public char [] user_type { get; set; }
+        public char user_type { get; set; }
     }
 }
