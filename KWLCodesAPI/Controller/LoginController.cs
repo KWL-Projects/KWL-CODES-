7b0c2a7619283db822afdbf87fde49059d340c53
Kaylaventer <venter.kayla@gmail.com>
2024-09-23 21:22:45 +0200

    Login Controller

---
+++ KWLCodesAPI/Controller/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KWLCodesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KWLCodesAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LoginController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public LoginController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Login>>> GetLogins()
+        {
+            return await _context.Login.ToListAsync();
+        }
+    }
+}
