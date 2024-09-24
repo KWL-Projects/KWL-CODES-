dc8b29154f13534905525330042f56299e559ebd
Kaylaventer <venter.kayla@gmail.com>
2024-09-24 13:54:33 +0200

    Create Login.cs

---
+++ KWL_HMSWeb/KWL_HMSWeb.Server/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KWL_HMSWeb.Server.Models
+{
+    public class Login
+    {
+        [Key]
+        public int login_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
