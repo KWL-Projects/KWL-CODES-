9e6a55c9f2d5e58c92109a5d529b63fa23372053
waldonieman <waldonieman@gmail.com>
2024-09-24 11:02:45 +0200

    Added Login Component To Web

--- KWL_HMSWeb/kwl_hmsweb.client/src/app/app.module.ts
+++ KWL_HMSWeb/kwl_hmsweb.client/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
