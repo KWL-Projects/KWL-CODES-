a179d1520e57a118b2da54ecdf63c9124aca90d6
waldonieman <waldonieman@gmail.com>
2024-09-25 09:15:58 +0200

    Login Button Fixed

--- KWL_HMSWeb/kwl_hmsweb.client/src/app/app.component.ts
+++ KWL_HMSWeb/kwl_hmsweb.client/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit {
+  isMainPage: boolean = false;
 
-export class AppComponent {
   constructor(private router: Router) { }
 
-  navigateToLogin() {
+  ngOnInit(): void {
+    this.checkRoute();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkRoute();
+      }
+    });
+  }
+
+  checkRoute(): void {
+    this.isMainPage = this.router.url === '/';
+    console.log('Current URL:', this.router.url); // Debugging line
+    console.log('isMainPage:', this.isMainPage); // Debugging line
+  }
+
+  navigateToLogin(): void {
     this.router.navigate(['/login']);
   }
 }
