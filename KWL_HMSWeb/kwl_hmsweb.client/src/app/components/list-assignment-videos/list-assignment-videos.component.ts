ef95f5822266a5955acbcebdf5abe9caa9dac71e
waldonieman <waldonieman@gmail.com>
2024-09-25 11:14:52 +0200

    Assignment Update

--- KWL_HMSWeb/kwl_hmsweb.client/src/app/components/list-assignment-videos/list-assignment-videos.component.ts
+++ KWL_HMSWeb/kwl_hmsweb.client/src/app/components/list-assignment-videos/list-assignment-videos.component.ts
@@ -2,16 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-list-assignment-videos',
-  templateUrl: './list-assignment-videos.component.html',
-  styleUrls: ['./list-assignment-videos.component.css']
+  selector: 'app-view-assignment-videos',
+  templateUrl: './view-assignment-videos.component.html',
+  styleUrls: ['./view-assignment-videos.component.css']
 })
 export class ListAssignmentVideosComponent implements OnInit {
   videos = [
-    { id: 1, title: 'Video 1' },
-    { id: 2, title: 'Video 2' }
+    {
+      id: 1,
+      title: 'Video 1',
+      submissionId: 1,
+      assignmentId: 101,
+      submissionDateTime: new Date('2024-09-20T10:00:00'),
+      submissionDescription: 'First submission description'
+    },
+    {
+      id: 2,
+      title: 'Video 2',
+      submissionId: 2,
+      assignmentId: 102,
+      submissionDateTime: new Date('2024-09-21T12:00:00'),
+      submissionDescription: 'Second submission description'
+    }
   ];
+  /*submissions: any[] = [];
 
+  constructor(private submissionService: SubmissionService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.submissionService.getSubmissions().subscribe(data => {
+      this.submissions = data;
+    });
+  }*/
   constructor(private router: Router) { }
 
   ngOnInit(): void {
