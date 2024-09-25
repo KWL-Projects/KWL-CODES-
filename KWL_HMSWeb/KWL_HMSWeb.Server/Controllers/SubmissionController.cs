89c6b5f436124945b634611c22e44f42bc444a4f
Kaylaventer <venter.kayla@gmail.com>
2024-09-25 14:54:21 +0200

    Controllers Created

---
+++ KWL_HMSWeb/KWL_HMSWeb.Server/Controllers/SubmissionController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KWL_HMSWeb.Server.Models;
+
+namespace KWL_HMSWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubmissionController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public SubmissionController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Submission
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Submission>>> GetSubmission()
+        {
+            return await _context.Submission.ToListAsync();
+        }
+
+        // GET: api/Submission/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Submission>> GetSubmission(int id)
+        {
+            var submission = await _context.Submission.FindAsync(id);
+
+            if (submission == null)
+            {
+                return NotFound();
+            }
+
+            return submission;
+        }
+
+        // PUT: api/Submission/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubmission(int id, Submission submission)
+        {
+            if (id != submission.submission_id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(submission).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubmissionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Submission
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Submission>> PostSubmission(Submission submission)
+        {
+            _context.Submission.Add(submission);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubmission", new { id = submission.submission_id }, submission);
+        }
+
+        // DELETE: api/Submission/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubmission(int id)
+        {
+            var submission = await _context.Submission.FindAsync(id);
+            if (submission == null)
+            {
+                return NotFound();
+            }
+
+            _context.Submission.Remove(submission);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SubmissionExists(int id)
+        {
+            return _context.Submission.Any(e => e.submission_id == id);
+        }
+    }
+}
