05cac336b21366a042284761f7f8cf2ee230fd75
Kaylaventer <venter.kayla@gmail.com>
2024-09-25 21:07:45 +0200

    Changes

--- KWLCodesAPI/Program.cs
+++ KWLCodesAPI/Program.cs
@@ -4,15 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+//builder.Services.AddDbContext<DatabaseContext>(options =>
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
