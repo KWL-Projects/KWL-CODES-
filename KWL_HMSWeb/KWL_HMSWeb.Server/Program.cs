6c6a20b1e4fdb4b940dad015da808ae0e92a0ba8
Kaylaventer <venter.kayla@gmail.com>
2024-09-27 12:21:15 +0200

    deleted test

--- KWL_HMSWeb/KWL_HMSWeb.Server/Program.cs
+++ KWL_HMSWeb/KWL_HMSWeb.Server/Program.cs
@@ -94,6 +94,10 @@ builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));*/
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
 builder.Services.AddScoped<IServices, VideoService>();
 
 builder.Services.AddSingleton<BlobStorageService>();
@@ -101,6 +105,16 @@ builder.Services.AddSingleton<BlobStorageService>();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
