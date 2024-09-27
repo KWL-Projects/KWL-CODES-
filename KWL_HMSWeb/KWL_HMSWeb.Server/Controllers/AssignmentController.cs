5b8316e7adab0b1f9f1627536160093b2830cb47
Kaylaventer <venter.kayla@gmail.com>
2024-09-27 11:42:35 +0200

    updates

--- KWL_HMSWeb/KWL_HMSWeb.Server/Controllers/AssignmentController.cs
+++ KWL_HMSWeb/KWL_HMSWeb.Server/Controllers/AssignmentController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using KWL_HMSWeb.Server.Models;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
 namespace KWL_HMSWeb.Server.Controllers
 {
     [Route("api/assignment")]
     [ApiController]
+    [Authorize(Roles = "admin,student,lecturer")]
     public class AssignmentController : ControllerBase
     {
         private readonly DatabaseContext _context;
