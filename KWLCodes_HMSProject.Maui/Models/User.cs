fb7734bf90b9005ba1351d292a3f03aad0610898
waldonieman <waldonieman@gmail.com>
2024-10-13 18:50:46 +0200

    Models and Services

---
+++ KWLCodes_HMSProject.Maui/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWLCodes_HMSProject.Maui.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }          // Primary Key
+        public string UserFirstname { get; set; } // User's first name
+        public string UserSurname { get; set; }    // User's surname
+        public string UserType { get; set; }       // User's role/type
+        public int LoginId { get; set; }           // Foreign Key
+       // public virtual Login login_id { get; set; }   // Navigation property
+    }
+
+}
